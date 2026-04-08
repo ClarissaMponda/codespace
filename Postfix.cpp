dc5535eb1f888e65cb07bf87494e9a968e2c79b0
ClarissaMponda <bsc-com-ne-30-24@unima.ac.mw>
2026-04-08 11:15:02 +0200

    Add files via upload

---
+++ Postfix.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int x = 3;
+
+// the value of x was assigned to y before incremented
+    
+    int y = x++;
+
+// output
+    cout <<"x: " << x << endl;
+    cout <<"y: " << y << endl;
+
+    return 0;
+}
